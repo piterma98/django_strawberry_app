30b407040fe8f58b889f3fc26bbea0d46781f049
piterma98 <piterma98@gmail.com>
2023-02-18 16:26:58 +0100

    Update tests

--- app/tests/test_graphql.py
+++ app/tests/test_graphql.py
@@ -10,20 +10,16 @@ def test_query(graphql_client):
                     name
                 }
             }
-            authors{
-                name
-                }
         }
     """
 
     result = graphql_client.query(
-        query,
+        query=query,
     )
 
     assert result.errors is None
     assert result.data == {
         "books": [{"title": "Jurassic Park", "author": {"name": "Michael Crichton"}}],
-        "authors": [{"name": "Michael Crichton"}],
     }
 
 
@@ -43,3 +39,41 @@ def test_me_query(user_factory, graphql_client):
 
     assert result.errors is None
     assert result.data == {"user": str(user)}
+
+
+@pytest.mark.django_db
+def test_authors_query(user_factory, graphql_client):
+    user = user_factory()
+    query = """
+        query Authors{
+                authors{
+            name
+            }
+        }
+    """
+
+    with graphql_client.login(user):
+        result = graphql_client.query(
+            query=query,
+        )
+
+    assert result.errors is None
+    assert result.data == {"authors": [{"name": "Michael Crichton"}]}
+
+
+@pytest.mark.django_db
+def test_authors_query_unauthorized(graphql_client):
+    query = """
+        query Authors{
+                authors{
+                    name
+            }
+        }
+    """
+
+    result = graphql_client.query(
+        query=query,
+        asserts_errors=False,
+    )
+
+    assert len(result.errors) == 1
